65ed13362dc25a9f782a89df152287d00ccad48b
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-01-22 11:24:33 +0100

    Enforce deltabar > rhobar > sigmabar in test DB

--- src/main/resources/import-test.sql
+++ src/main/resources/import-test.sql
@@ -1,5 +1,5 @@
 INSERT INTO PROVIDER(p_id) VALUES('Amazon');
 INSERT INTO PROVIDER(p_id) VALUES('Flexi');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(1,0.9, 'Amazon',0.4,2.1,'T1');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(3,1.4, 'Amazon',0.5,2.3,'T2');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(4,1.0, 'Flexi',0.4,2.1,'T3');
+INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(1,2.1, 'Amazon',0.9,0.4,'T1');
+INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(3,1.4, 'Amazon',1.1,0.3,'T2');
+INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(4,2.0, 'Flexi',1.4,0.5,'T3');
