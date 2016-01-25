13e77f14baf778fcb6cf0c626a30089c8d58ec8c
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-01-25 12:45:23 +0100

    Avoid test failures if there is something in /home/tueguem on ch14r4

--- src/test/java/it/polimi/diceH2020/SPACE4CloudWS/test/connection/TestConnection.java
+++ src/test/java/it/polimi/diceH2020/SPACE4CloudWS/test/connection/TestConnection.java
@@ -1,7 +1,7 @@
 package it.polimi.diceH2020.SPACE4CloudWS.test.connection;
 
-import java.util.List;
-
+import it.polimi.diceH2020.SPACE4CloudWS.solvers.MINLPSolver;
+import it.polimi.diceH2020.SPACE4CloudWS.solvers.SPNSolver;
 import org.aspectj.apache.bcel.util.ClassPath;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,46 +11,44 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import it.polimi.diceH2020.SPACE4CloudWS.solvers.MINLPSolver;
-import it.polimi.diceH2020.SPACE4CloudWS.solvers.SPNSolver;
+import java.util.List;
 
-@RunWith(SpringJUnit4ClassRunner.class)  
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = it.polimi.diceH2020.SPACE4CloudWS.main.SPACE4CloudWS.class)   // 2
 @ActiveProfiles("test")
 public class TestConnection {
-		@Autowired
-		MINLPSolver milpSolver;	
-		
-		@Autowired
-		SPNSolver spnSolver;
+	@Autowired
+	MINLPSolver milpSolver;
 
-	    @Test
-	    public void testApplDataFormat() throws Exception {
+	@Autowired
+	SPNSolver spnSolver;
 
-				List<String> res = milpSolver.clearWorkingDir();
-				Assert.assertTrue(res.contains("exit-status: 0"));
-				res.clear();
-				res = milpSolver.getConnector().exec("ls");
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-				String wd = milpSolver.getRemoteWorkingDirectory();
-				res = milpSolver.getConnector().exec("mkdir " + wd);
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-				res = milpSolver.getConnector().exec("cd " + wd);
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-				System.out.println(milpSolver.pwd());
-				res = milpSolver.getConnector().exec("cd " + wd + " && mkdir problems utils solve");
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-				System.out.println(ClassPath.getClassPath());
-	    }
-	    
-	    @Test
-	    public void testSPN() throws Exception{
-				List<String> res = spnSolver.pwd();
-				Assert.assertTrue(res.size() == 2 && res.get(0).contains("/home/user") && res.contains("exit-status: 0"));
-				res = spnSolver.getConnector().exec("rm -rf ./Experiments");
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-				res = spnSolver.getConnector().exec("mkdir ./Experiments");
-				Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
-	    }
-	    
+	@Test
+	public void testAMPL() throws Exception {
+		List<String> res = milpSolver.clearWorkingDir();
+		Assert.assertTrue(res.contains("exit-status: 0"));
+		res.clear();
+		res = milpSolver.getConnector().exec("ls");
+		Assert.assertTrue(res.contains("exit-status: 0"));
+		String wd = milpSolver.getRemoteWorkingDirectory();
+		res = milpSolver.getConnector().exec("mkdir " + wd);
+		Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
+		res = milpSolver.getConnector().exec("cd " + wd);
+		Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
+		System.out.println(milpSolver.pwd());
+		res = milpSolver.getConnector().exec("cd " + wd + " && mkdir problems utils solve");
+		Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
+		System.out.println(ClassPath.getClassPath());
+	}
+
+	@Test
+	public void testSPN() throws Exception{
+		List<String> res = spnSolver.pwd();
+		Assert.assertTrue(res.size() == 2 && res.get(0).contains("/home/user") && res.contains("exit-status: 0"));
+		res = spnSolver.getConnector().exec("rm -rf ./Experiments");
+		Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
+		res = spnSolver.getConnector().exec("mkdir ./Experiments");
+		Assert.assertTrue(res.size() == 1 && res.contains("exit-status: 0"));
+	}
+
 }
