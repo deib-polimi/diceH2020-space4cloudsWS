73791abd4ea72c8b51a2bf9ca43341535091d21f
miciav <michele.ciavotta@gmail.com>
2016-02-19 00:03:08 +0100

    controllers protected during the optimization

--- src/main/java/it/polimi/diceH2020/SPACE4CloudWS/controller/Controller.java
+++ src/main/java/it/polimi/diceH2020/SPACE4CloudWS/controller/Controller.java
@@ -23,38 +23,37 @@ public class Controller {
 	@RequestMapping(method = RequestMethod.POST, value = "/event")
 	public @ResponseBody String changeState(@RequestBody Events event) throws Exception {
 
-		
 		stateHandler.sendEvent(event);
 		States currentState = stateHandler.getState().getId();
-		if (currentState.equals(States.RUNNING_INIT))
-			engineService.runningInitSolution();
-		if (currentState.equals(States.RUNNING_LS))
-			engineService.localSearch();
-		
-		// if the state transitions to RUNNING_initLocaSearch Spring will call
-		// EngineService.runningInitLocalSearch
+		if (currentState.equals(States.RUNNING_INIT)) engineService.runningInitSolution();
+		if (currentState.equals(States.RUNNING_LS)) engineService.localSearch();
+
 		return getWebServiceState();
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/settings")
 	public @ResponseBody String changeState(@RequestBody Settings settings) {
-		engineService.setAccuracyAndCycles(settings);
+		if (getWebServiceState().equals("IDLE")) engineService.setAccuracyAndCycles(settings);
 		return getWebServiceState();
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/inputdata")
 	@ResponseStatus(value = HttpStatus.OK)
 	public String endpointInputData(@RequestBody InstanceData inputData) throws Exception {
-		engineService.setInstanceData(inputData);
-		stateHandler.sendEvent(Events.TO_CHARGED_INPUTDATA);
+		if (getWebServiceState().equals("IDLE")) {
+			engineService.setInstanceData(inputData);
+			stateHandler.sendEvent(Events.TO_CHARGED_INPUTDATA);
+		}
 		return getWebServiceState();
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/solution")
 	@ResponseStatus(value = HttpStatus.OK)
 	public String endpointSolution(@RequestBody Solution sol) throws Exception {
-		engineService.setSolution(sol);
-		stateHandler.sendEvent(Events.TO_CHARGED_INITSOLUTION);
+		if (getWebServiceState().equals("IDLE")) {
+			engineService.setSolution(sol);
+			stateHandler.sendEvent(Events.TO_CHARGED_INITSOLUTION);
+		}
 		return getWebServiceState();
 	}
 
@@ -62,8 +61,7 @@ public class Controller {
 	@ResponseStatus(value = HttpStatus.OK)
 	public Solution endpointSolution() throws Exception {
 		String state = stateHandler.getState().getId().toString();
-		if (state.equals("CHARGED_INITSOLUTION") || state.equals("FINISH"))
-			return engineService.getSolution();
+		if (state.equals("CHARGED_INITSOLUTION") || state.equals("FINISH")) return engineService.getSolution();
 		return null;
 	}
 
