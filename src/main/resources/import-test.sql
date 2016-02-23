fea999d77068379dda78046a25abca32e1f91479
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2016-02-23 10:41:02 +0100

    Update internal VM type database

--- src/main/resources/import-test.sql
+++ src/main/resources/import-test.sql
@@ -1,5 +1,7 @@
 INSERT INTO PROVIDER(p_id) VALUES('Amazon');
-INSERT INTO PROVIDER(p_id) VALUES('Flexi');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(2,2.1, 'Amazon',0.9,0.4,'T1');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(4,1.4, 'Amazon',1.1,0.3,'T2');
-INSERT INTO TYPEVM(core, deltabar, p_id, rhobar,sigmabar,type) VALUES(4,2.0, 'Flexi',1.4,0.5,'T3');
+INSERT INTO PROVIDER(p_id) VALUES('Cineca');
+INSERT INTO TYPEVM(core,deltabar,p_id,rhobar,sigmabar,type) VALUES(1,0.073,'Amazon',0.0496,0.0112,'medium');
+INSERT INTO TYPEVM(core,deltabar,p_id,rhobar,sigmabar,type) VALUES(2,0.146,'Amazon',0.1002,0.0211,'large');
+INSERT INTO TYPEVM(core,deltabar,p_id,rhobar,sigmabar,type) VALUES(4,0.293,'Amazon',0.1994,0.0417,'xlarge');
+INSERT INTO TYPEVM(core,deltabar,p_id,rhobar,sigmabar,type) VALUES(8,0.5280,'Amazon',0.3352,0.0666,'2xlarge');
+INSERT INTO TYPEVM(core,deltabar,p_id,rhobar,sigmabar,type) VALUES(20,1.3205,'Cineca',0.8379,0.1650,'5xlarge');
