56a7d2fb2c5397a227942ba711dae6b3e77e8d87
jacoporigoli <jackeape@gmail.com>
2016-10-13 15:24:53 +0200

    [MLPredictor] Fixs, logger

--- src/main/java/it/polimi/diceH2020/SPACE4CloudWS/ml/MLPredictor.java
+++ src/main/java/it/polimi/diceH2020/SPACE4CloudWS/ml/MLPredictor.java
@@ -20,6 +20,8 @@ import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.JobProfile;
 import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.SVRFeature;
 import it.polimi.diceH2020.SPACE4Cloud.shared.solution.SolutionPerJob;
 import it.polimi.diceH2020.SPACE4CloudWS.services.DataService;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,51 +33,61 @@ import java.util.Optional;
 @Service
 public class MLPredictor {
 
+	private final Logger logger = Logger.getLogger(getClass());
+
 	@Autowired
 	private DataService dataService;
 
-	private Map<String,MLPrediction> predictedSPJ = new HashMap<>();//caches SPJ, in order to recalculate only xi TODO @Cacheable 
+	private Map<String, MLPrediction> predictedSPJ = new HashMap<>();// caches
+																		// SPJ,
+																		// in
+																		// order
+																		// to
+																		// recalculate
+																		// only
+																		// xi
+																		// TODO
+																		// @Cacheable
 
 	/**
-	 * Precondition:
-	 * DataService has M,V (received from JSON or retrieved from DB)
-	 * SolutionPerJob has h,m,v,D and all the required parameters in JobMLProfile
+	 * Precondition: DataService has M,V (received from JSON or retrieved from
+	 * DB) SolutionPerJob has h,m,v,D and all the required parameters in
+	 * JobMLProfile
 	 *
 	 * @param spj
 	 * @return
 	 */
 	public Optional<BigDecimal> approximate(SolutionPerJob spj) {
-		if(predictedSPJ.containsKey(spj.getId())){
+		if (predictedSPJ.containsKey(spj.getId())) {
 			return retrievePrediction(spj);
-		}else{
+		} else {
 			return calculatePrediction(spj);
 		}
 	}
 
-
-	private Optional<BigDecimal> calculatePrediction(SolutionPerJob spj){
+	private Optional<BigDecimal> calculatePrediction(SolutionPerJob spj) {
 		JobMLProfile features = dataService.getMLProfile(spj.getId());
 		JobProfile profile = spj.getProfile();
 
 		double deadline = spj.getJob().getD();
 		double chi_c = calculateChi_c(features);
 		double chi_h = calculateChi_h(features);
-		double chi_0 = calculateChi_0(profile,features);
+		double chi_0 = calculateChi_0(profile, features);
 		int h = spj.getNumberUsers();
 
-		double duration = deadline; //TODO
+		double duration = deadline; // TODO
 		double xi = calculateXi(spj);
-		int c = (int) Math.ceil((double)(chi_c / (deadline - chi_h*h - chi_0)));
+		int c = (int) Math.ceil((double) (chi_c / (deadline - chi_h * h - chi_0)));
 
 		spj.setXi(xi);
 		spj.setDuration(duration);
-		spj.setNumberContainers(c); //TODO
-
-		predictedSPJ.put(spj.getId(), new MLPrediction(deadline,chi_c,chi_h,chi_0));
+		spj.updateNumberContainers(c); // TODO
+		logger.debug("numContainers = ceil(chi_c/(deadline - chi_h*h - chi_0)) = ceil("+chi_c+"/("+deadline+"-"+chi_h+"*"+h+"-"+chi_0+") = "+c);
+		predictedSPJ.put(spj.getId(), new MLPrediction(deadline, chi_c, chi_h, chi_0));
 		return Optional.of(BigDecimal.valueOf(duration));
 	}
 
-	private Optional<BigDecimal> retrievePrediction(SolutionPerJob spj){
+	private Optional<BigDecimal> retrievePrediction(SolutionPerJob spj) {
 		MLPrediction prediction = predictedSPJ.get(spj.getId());
 
 		double deadline = prediction.getDeadline();
@@ -85,19 +97,21 @@ public class MLPredictor {
 
 		int h = spj.getNumberUsers();
 
-		double duration = deadline; //TODO
+		double duration = deadline; // TODO
 		double xi = calculateXi(spj);
-		int c = (int) Math.ceil((double)(chi_c / (deadline - chi_h*h - chi_0)));
-
+		int c = (int) Math.ceil((double) (chi_c / (deadline - chi_h * h - chi_0)));
+		
+		logger.debug("numContainers = ceil(chi_c/(deadline - chi_h*h - chi_0)) = ceil("+chi_c+"/("+deadline+"-"+chi_h+"*"+h+"-"+chi_0+") = "+c);
 		spj.setXi(xi);
 		spj.setDuration(duration);
-		spj.setNumberContainers(c); //TODO
-
+		spj.updateNumberContainers(c); // TODO
+		
 		return Optional.of(BigDecimal.valueOf(duration));
 	}
 
-	private double calculateDefaultParametersContribution(JobMLProfile features){
-		//mu_t + b*sigma_t - (sigma_t/sigma_x)*w_x*mu_x - (sigma_t/sigma_h)*w_h*mu_h 
+	private double calculateDefaultParametersContribution(JobMLProfile features) {
+		// mu_t + b*sigma_t - (sigma_t/sigma_x)*w_x*mu_x -
+		// (sigma_t/sigma_h)*w_h*mu_h
 		double mu_t = features.getMu_t();
 		double sigma_t = features.getSigma_t();
 		double b = features.getB();
@@ -108,53 +122,65 @@ public class MLPredictor {
 		double mu_h = features.getClassFeature("h").getMu();
 		double w_h = features.getClassFeature("h").getW();
 
-		return mu_t + b*sigma_t - (sigma_t/sigma_x)*w_x*mu_x - (sigma_t/sigma_h)*w_h*mu_h;
+		double result = mu_t + b * sigma_t - (sigma_t / sigma_x) * w_x * mu_x - (sigma_t / sigma_h) * w_h * mu_h;
+		logger.debug(
+				"Chi_0_mandatoryParameters = mu_t + b*sigma_t - (sigma_t/sigma_x)*w_x*mu_x - (sigma_t/sigma_h)*w_h*mu_h = "
+						+ mu_t + "+" + b + "*" + sigma_t + "- (" + sigma_t + "/" + sigma_x + ")*" + w_x + "*" + mu_x
+						+ "- (" + sigma_t + "/" + sigma_h + ")*" + w_h + "*" + mu_h + " \n= " + result);
+		return result;
 	}
 
-
-	private double calculateChi_c(JobMLProfile features){
+	private double calculateChi_c(JobMLProfile features) {
 		double sigma_t = features.getSigma_t();
 		double sigma_x = features.getClassFeature("x").getSigma();
 		double w_x = features.getClassFeature("x").getW();
 
-		return  (sigma_t/sigma_x)*w_x;
+		double result = (sigma_t / sigma_x) * w_x;
+		logger.debug("Chi_c = (sigma_t/sigma_x)*w_x = (" + sigma_t + "/" + sigma_x + ")*" + w_x + " = " + result);
+		return result;
 	}
 
-	private double calculateChi_h(JobMLProfile features){
+	private double calculateChi_h(JobMLProfile features) {
 		double sigma_t = features.getSigma_t();
 		double sigma_h = features.getClassFeature("h").getSigma();
 		double w_h = features.getClassFeature("h").getW();
-
-		return  (sigma_t/sigma_h)*w_h;
+		double result = (sigma_t / sigma_h) * w_h;
+		logger.debug("Chi_h = (sigma_t/sigma_h)*w_h = (" + sigma_t + "/" + sigma_h + ")*" + w_h + " = " + result);
+		return result;
 	}
 
-	private double calculateXi(SolutionPerJob spj){
+	private double calculateXi(SolutionPerJob spj) {
 		double M = dataService.getMemory(spj.getTypeVMselected().getId());
 		double m = spj.getJob().getM();
 		double V = dataService.getNumCores(spj.getTypeVMselected().getId());
 		double v = spj.getJob().getV();
-		double xi = Math.min(M/m,V/v);
-
+		double xi = Math.min(M / m, V / v);
+		logger.debug("xi = min(M/m,V/v) = min(" + M + "/" + m + "," + V + "/" + v + ") = " + xi);
 		return xi;
 	}
 
-	private double calculateChi_0(JobProfile profile,JobMLProfile features){
+	private double calculateChi_0(JobProfile profile, JobMLProfile features) {
 
 		double defaultParametersContribution = calculateDefaultParametersContribution(features);
 		double featureContribution = 0;
-
+		String chi_0_optional_names = new String();
+		String chi_0_optional_values = new String();
 		try {
-			for (Map.Entry<String,SVRFeature> entry : features.getMlFeatures().entrySet()) {
-				if(entry.getKey().equals("h")||entry.getKey().equals("x")) continue;
-
+			for (Map.Entry<String, SVRFeature> entry : features.getMlFeatures().entrySet()) {
+				if (entry.getKey().equals("h") || entry.getKey().equals("x"))
+					continue;
 				double valueOfEntry = profile.get(entry.getKey());
-				featureContribution += (features.getSigma_t()/entry.getValue().getSigma())*entry.getValue().getW()*(valueOfEntry-entry.getValue().getMu());
+				chi_0_optional_names += "+ (sigma_t/sigma_"+entry.getKey()+")*w_"+entry.getKey()+"*("+entry.getKey()+"-"+"mu_"+entry.getKey()+")";
+				chi_0_optional_values += "+("+features.getSigma_t()+" / "+entry.getValue().getSigma()+") * "+entry.getValue().getW()+"*("+valueOfEntry+ "-"+ entry.getValue().getMu()+")";
+				featureContribution += (features.getSigma_t() / entry.getValue().getSigma()) * entry.getValue().getW()*(valueOfEntry - entry.getValue().getMu());
 			}
 		} catch (IllegalArgumentException e) {
-			System.out.println("[MLPredictor] Missing a MLProfile feature parameter in Profile.");
+			logger.info("[MLPredictor] Missing a MLProfile feature parameter in Profile.");
 		}
-
-		return defaultParametersContribution + featureContribution ;
+		logger.debug("Chi_0_optional_parameters: "+chi_0_optional_names+"\n"+"="+chi_0_optional_values+"\n = "+featureContribution);
+		double result = defaultParametersContribution + featureContribution; 
+		logger.debug("Chi_0 = Chi_0_mandatory_parameters + Chi_0_optional_parameters  = "+defaultParametersContribution+"+"+featureContribution +" = "+result);
+		return result;
 	}
 
 	public void reinitialize() {
