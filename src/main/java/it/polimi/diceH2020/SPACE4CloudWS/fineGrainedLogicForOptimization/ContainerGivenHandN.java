67fcce2a95c9082bd528cd8033a11b81590f93cd
Eugenio Gianniti <eugenio.gianniti@polimi.it>
2017-02-10 16:59:43 +0100

    Add missing copyright notices

--- src/main/java/it/polimi/diceH2020/SPACE4CloudWS/fineGrainedLogicForOptimization/ContainerGivenHandN.java
+++ src/main/java/it/polimi/diceH2020/SPACE4CloudWS/fineGrainedLogicForOptimization/ContainerGivenHandN.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 Jacopo Rigoli
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package it.polimi.diceH2020.SPACE4CloudWS.fineGrainedLogicForOptimization;
 
 import it.polimi.diceH2020.SPACE4Cloud.shared.solution.SolutionPerJob;
@@ -7,7 +22,7 @@ public abstract class ContainerGivenHandN {
 	public ContainerLogicGivenH getHandler() {
 		return null;
 	}
-	
+
 	public SolutionPerJob getSpj() {
 		return null;
 	}
--- src/main/java/it/polimi/diceH2020/SPACE4CloudWS/services/Validator.java
+++ src/main/java/it/polimi/diceH2020/SPACE4CloudWS/services/Validator.java
@@ -1,16 +1,30 @@
+/*
+Copyright 2016 Jacopo Rigoli
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package it.polimi.diceH2020.SPACE4CloudWS.services;
 
+import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.InstanceDataMultiProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider.InstanceDataMultiProvider;
-
 @Service
 public class Validator {
-	
+
 	@Autowired
 	private DataService dataService;
-	
+
 	/**
 	 * Set in DataService: <br>
 	 * &emsp; -inputData <br>
